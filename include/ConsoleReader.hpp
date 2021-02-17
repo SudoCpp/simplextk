546d3019a60934c6ac2ee935c9f12d10e0fecda8
SudoCpp <SudoCpp@gmail.com>
2021-02-16 22:29:39 -0600

    v2.0 Squashed commit of the following:
    commit 1ab89e99c4e544aac68378581c7f066d55acc64e
    Author: SudoCpp <SudoCpp@gmail.com>
    Date:   Tue Feb 16 22:29:21 2021 -0600
    
        Added CMake Support to Console.
    
    commit f6d19e68273517baf51acc8a94f95cdcc92b79eb
    Author: SudoCpp <SudoCpp@gmail.com>
    Date:   Tue Feb 16 22:28:03 2021 -0600
    
        Adding a "static" class for Console.
    
    commit e3cf00ba2aa5a3f0b2c19afb33f5c11a44c9bee1
    Author: SudoCpp <SudoCpp@gmail.com>
    Date:   Tue Feb 16 22:26:09 2021 -0600
    
        Adding CMake support for console streams.
    
    commit 9ab66d768113a0430522111605f03174012c0507
    Author: SudoCpp <SudoCpp@gmail.com>
    Date:   Tue Feb 16 22:24:26 2021 -0600
    
        Adding a console writer.
    
    commit 730aa97af807c3631da0b3aab0b3be28ca267e35
    Author: SudoCpp <SudoCpp@gmail.com>
    Date:   Tue Feb 16 22:23:26 2021 -0600
    
        Adding a console reader.
    
    commit d19157c481b1846cf307a04e712de733f7ff6391
    Author: SudoCpp <SudoCpp@gmail.com>
    Date:   Tue Feb 16 22:16:47 2021 -0600
    
        Beginning to add streams.

---
+++ include/ConsoleReader.hpp
@@ -0,0 +1,54 @@
+/*
+    BSD 3-Clause License
+    
+    Copyright (c) 2021, SudoCpp
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+    3. Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef SIMPLEX_CONSOLEREADER_HPP
+#define SIMPLEX_CONSOLEREADER_HPP
+
+#include "StreamReader.hpp"
+#include <sstream>
+
+namespace simplex
+{
+    class string;
+	class ConsoleReader : public StreamReader
+	{
+        mutable std::stringstream buffer;
+		
+        public:
+        virtual bool read(unsigned int numberOfCharacters, string& readTo) const;
+        virtual string readLine() const;
+        virtual void rewind(unsigned int numberOfCharacters) const;
+        virtual void rewind() const;
+	};
+}
+
+#endif // SIMPLEX_CONSOLEREADER_HPP
