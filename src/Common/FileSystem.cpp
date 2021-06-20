ab1bfae11e545978ee255f09686f2fa624e341ff
SudoCpp <SudoCpp@gmail.com>
2021-06-20 11:14:51 -0500

    Adding the exists method.

--- src/Common/FileSystem.cpp
+++ src/Common/FileSystem.cpp
@@ -1,11 +1,26 @@
 #include "FileSystem.hpp"
 #include "Exception.hpp"
 
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #define __class__ "simplex::FileSystem"
 
 namespace simplex::FileSystem
 {
-    string getExtension(string fullPath)
+    bool exists(const string& path)
+    {
+        struct stat info;
+
+        if(stat(path.toCString(), &info) != 0 )
+            return false;
+        else if(info.st_mode & S_IFDIR)
+            return true;
+        else
+            return false;
+    }
+
+    string getExtension(const string& fullPath)
     {
         int position = fullPath.lastIndexOf(".");
         if(position == -1)
