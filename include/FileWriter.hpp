4320f67890f83c7408126596f8d0f1e5b94295dc
SudoCpp <SudoCpp@gmail.com>
2022-01-09 23:39:11 -0600

    Update Copyright year.

--- include/FileWriter.hpp
+++ include/FileWriter.hpp
@@ -1,7 +1,7 @@
 /*
     BSD 3-Clause License
     
-    Copyright (c) 2021, SudoCpp
+    Copyright (c) 2022, SudoCpp
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
