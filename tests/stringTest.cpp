b288a9cd340f598744ec7b8b9aa77c11aaa73406
SudoCpp <SudoCpp@gmail.com>
2022-01-26 20:02:26 -0600

    Align tests with updated classes.

--- tests/stringTest.cpp
+++ tests/stringTest.cpp
@@ -1,3 +1,35 @@
+/*
+    BSD 3-Clause License
+    
+    Copyright (c) 2022, SudoCpp
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+    3. Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include "string.hpp"
 #include <iostream>
 
@@ -54,14 +86,15 @@ int main(int argc, char* argv[])
         else
             return EXIT_FAILURE;
     }
-    // else if(convert == "6")
-    // {
-    //     simplex::string testString{"ATESTTEST"};
-    //     if(testString.lastIndexOf("TEST", 9) == 2)
-    //         return EXIT_SUCCESS;
-    //     else
-    //         return EXIT_FAILURE;
-    // }
+    else if(convert == "6")
+    {
+        // simplex::string testString1{"TEST"};
+        // simplex::String testString2
+        // if()
+            return EXIT_SUCCESS;
+        // else
+        //     return EXIT_FAILURE;
+    }
     else if(convert == "7")
     {
         simplex::string testString{"ATESTTEST"};
@@ -113,14 +146,22 @@ int main(int argc, char* argv[])
     }
     else if(convert == "13")
     {
-        //if(simplex::string{"Test"}.toString() == simplex::string{"Test"})
+        if(simplex::string{"  Test  "}.trim() == simplex::string{"Test"})
             return EXIT_SUCCESS;
-        //else
-          //  return EXIT_FAILURE;
+        else
+           return EXIT_FAILURE;
     }
     else if(convert == "14")
     {
         if(simplex::string::FromNumber(12) + simplex::string::FromNumber(34) == simplex::string{"1234"})
+            return EXIT_SUCCESS;
+        else
+            return EXIT_FAILURE;
+    }
+    else if(convert == "15")
+    {
+        float number = 12.34;
+        if(simplex::string::FromNumber(number, 2) == simplex::string{"12.34"})
             return EXIT_SUCCESS;
         else
             return EXIT_FAILURE;
