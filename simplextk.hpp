88533cdb62e4c7d4b57cff8f4586f422d4e7c5c1
SudoCpp <SudoCpp@gmail.com>
2022-02-27 13:08:26 -0600

    Adding combined header for easy calling.

---
+++ simplextk.hpp
@@ -0,0 +1,70 @@
+/*
+    BSD 3-Clause License
+    
+    Copyright (c) 2022, SudoCpp
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+    3. Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef SIMPLEXTK_HPP
+#define SIMPLEXTK_HPP
+
+#include "simplextk/include/Array.hpp"
+#include "simplextk/include/Console.hpp"
+#include "simplextk/include/ConsoleReader.hpp"
+#include "simplextk/include/ConsoleWriter.hpp"
+#include "simplextk/include/Database.hpp"
+#include "simplextk/include/DataTable.hpp"
+#include "simplextk/include/DateTime.hpp"
+#include "simplextk/include/Dictionary.hpp"
+#include "simplextk/include/Exception.hpp"
+#include "simplextk/include/FileReader.hpp"
+#include "simplextk/include/FileSystem.hpp"
+#include "simplextk/include/FileWriter.hpp"
+#include "simplextk/include/Internal.hpp"
+#include "simplextk/include/Logger.hpp"
+#include "simplextk/include/Math.hpp"
+#include "simplextk/include/object.hpp"
+#include "simplextk/include/Primitive.hpp"
+#include "simplextk/include/Signal.hpp"
+#include "simplextk/include/Singleton.hpp"
+#include "simplextk/include/SingletonManager.hpp"
+#include "simplextk/include/SmartRef.hpp"
+#include "simplextk/include/StreamReader.hpp"
+#include "simplextk/include/StreamWriter.hpp"
+#include "simplextk/include/string.hpp"
+#include "simplextk/include/StringReader.hpp"
+#include "simplextk/include/StringWriter.hpp"
+#include "simplextk/include/SupportsSignals.hpp"
+#include "simplextk/include/TreeNode.hpp"
+#include "simplextk/include/Tuple.hpp"
+#include "simplextk/include/TypeArray.hpp"
+#include "simplextk/include/XmlReader.hpp"
+#include "simplextk/include/XmlWriter.hpp"
+
+#undef _internal
+#endif //SIMPLEXTK_HPP
